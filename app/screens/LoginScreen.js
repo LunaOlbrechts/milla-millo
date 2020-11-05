180b04bb90e0bd67207eaf4871b4b74be25d2711
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-11-05 21:11:12 +0100

    setup React Native

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,13 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button, Alert } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, Button, Alert,TextInput } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Constants from "expo-constants";
+const { manifest } = Constants
+
+
+const Stack = createStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={WelcomeScreen} options={{ title: 'Login' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const postLoginData = (value) => {
+        var userdata = {'name': value}
+
+        console.log(userdata);
+            
+        fetch("http://192.168.0.159:8000/login", {
+            method: "POST",
+            headers: new Headers({
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json'
+                }),
+            body:  userdata
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json.status);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+   
+};
 
 function LoginScreen({ navigation }) {
+        const [value, onChangeText] = React.useState('name');
     return (
        <SafeAreaView style={styles.container}>
            <Text>Inloggen</Text>
+
+           <TextInput
+                style={{ height: 40, width:100, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={text => onChangeText(text)}
+                value={value}
+            />
+
+            <Button
+              title="Login"
+              onPress={() => postLoginData(value)}
+            />        
        </SafeAreaView> 
     );
 }
